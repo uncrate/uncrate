c7394a5b92d03a7a68187ec9ed308483994d4099
John Leacox <johnlcox@gmail.com>
2013-07-01 07:42:51 -0500

    Create metadata.rb

---
+++ germinator-linux-apps/metadata.rb
@@ -0,0 +1,6 @@
+name             'germinator-linux-apps'
+maintainer       'John Leacox'
+license          'Apache 2'
+description      'Installs/Configures germinator-linux-apps'
+version          '0.1.0'
+supports         'Linux'
