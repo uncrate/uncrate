b423013180a2f64186547a7d73b1c61351300af9
John Leacox <jleacox@stackify.com>
2013-07-03 10:10:25 -0500

    Fix download url

--- germinator_linux_apps/attributes/intellij_community_edition.rb
+++ germinator_linux_apps/attributes/intellij_community_edition.rb
@@ -1,4 +1,4 @@
-default[:intellij_community_edition][:download_url]="http://download.jetbrains.com/idea/ideaIC_12.1.4.tar.gz"
+default[:intellij_community_edition][:download_url]="http://download.jetbrains.com/idea/ideaIC-12.1.4.tar.gz"
 default[:intellij_community_edition][:checksum]="d1307a88263147ab537b5ed61acc878ead6ca31422dc8ac652fbb551bd9300cd"
 default[:intellij_community_edition][:name]="IntelliJ IDEA 12 CE"
 default[:intellij_community_edition][:install_path]="/opt/intellij-ce"
