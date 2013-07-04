ea5165edeb5a59566ea7996a2706f75a5f94347b
John Leacox <johnlcox@gmail.com>
2013-07-04 17:51:01 -0500

    Replace symbols with strings

--- germinator_linux_apps/attributes/intellij_community_edition.rb
+++ germinator_linux_apps/attributes/intellij_community_edition.rb
@@ -1,6 +1,6 @@
-default[:intellij_community_edition][:download_url]="http://download.jetbrains.com/idea/ideaIC-12.1.4.tar.gz"
-default[:intellij_community_edition][:checksum]="d1307a88263147ab537b5ed61acc878ead6ca31422dc8ac652fbb551bd9300cd"
-default[:intellij_community_edition][:name]="IntelliJ IDEA 12 CE"
-default[:intellij_community_edition][:install_path]="/opt/intellij-ce"
+default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-12.1.4.tar.gz"
+default['intellij_community_edition']['checksum']="d1307a88263147ab537b5ed61acc878ead6ca31422dc8ac652fbb551bd9300cd"
+default['intellij_community_edition']['name']="IntelliJ IDEA 12 CE"
+default['intellij_community_edition']['install_path']="/opt/intellij-ce"
 
-default[:intellij_community_edition][:shortcut_name]="intellij-ce"
+default['intellij_community_edition']['shortcut_name']="intellij-ce"
--- germinator_linux_apps/recipes/intellij_community_edition.rb
+++ germinator_linux_apps/recipes/intellij_community_edition.rb
@@ -1,10 +1,10 @@
-intellij_tar_filename = File.basename(node[:intellij_community_edition][:download_url])
-intellij_tar_filepath = "#{Chef::Config[:file_cach_path]}/#{intellij_tar_filename}"
-intellij_extract_path = "#{Chef::Config[:file_cach_path]}/intellij_community_edition/#{node[:intellij_community_edition][:checksum]}"
+intellij_tar_filename = File.basename(node['intellij_community_edition']['download_url'])
+intellij_tar_filepath = "#{Chef::Config['file_cach_path']}/#{intellij_tar_filename}"
+intellij_extract_path = "#{Chef::Config['file_cach_path']}/intellij_community_edition/#{node['intellij_community_edition']['checksum']}"
 
 remote_file intellij_tar_filepath do
-  source node[:intellij_community_edition][:download_url]
-  checksum node[:intellij_community_edition][:checksum]
+  source node['intellij_community_edition']['download_url']
+  checksum node['intellij_community_edition']['checksum']
   owner "root"
   group "root"
   mode "0644"
@@ -15,14 +15,14 @@ bash "extract_intellij_community_edition" do
   code <<-EOH
     mkdir -p #{intellij_extract_path}
     tar zxf #{intellij_tar_filename} -C #{intellij_extract_path}
-    mkdir -p #{node[:intellij_community_edition][:install_path]}
-    mv #{intellij_extract_path}/*/* #{node[:intellij_community_edition][:install_path]}/
+    mkdir -p #{node['intellij_community_edition']['install_path']}
+    mv #{intellij_extract_path}/*/* #{node['intellij_community_edition']['install_path']}/
   EOH
 
-  not_if { ::File.exists?(node[:intellij_community_edition][:install_path])}
+  not_if { ::File.exists?(node['intellij_community_edition']['install_path'])}
 end
 
-template "/usr/share/applications/#{node[:intellij_community_edition][:shortcut_name]}.desktop" do
+template "/usr/share/applications/#{node['intellij_community_edition']['shortcut_name']}.desktop" do
   source "intellij_community_edition.desktop.erb"
   cookbook "germinator_linux_apps"
   owner node['current_user']
