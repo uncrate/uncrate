ef53d862bfe393c778068f1524b1f0c37d8c75ae
John Leacox <johnlcox@gmail.com>
2013-07-31 20:59:02 -0500

    Update scala download url

--- germinator_linux_apps/attributes/scala.rb
+++ germinator_linux_apps/attributes/scala.rb
@@ -1,3 +1,3 @@
-default['scala']['download_url']="http://www.scala-lang.org/downloads/distrib/files/scala-2.10.2.tgz"
+default['scala']['download_url']="http://www.scala-lang.org/files/archive/scala-2.10.2.tgz"
 default['scala']['checksum']="f693fdbc4944a40a080866593c70c14253fca2f553a0c50a26c8c52c489666a3"
 default['scala']['install_dir']="/opt"
