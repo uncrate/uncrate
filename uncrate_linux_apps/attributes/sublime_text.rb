fb97613ca0b4e87e20cf5b6577cab2fa43a971de
John Leacox <johnlcox@gmail.com>
2013-08-04 04:07:30 -0500

    Refactor sublime recipe

--- uncrate_linux_apps/attributes/sublime_text.rb
+++ uncrate_linux_apps/attributes/sublime_text.rb
@@ -1,6 +1,7 @@
 default['sublime_text']['download_url']="http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2"
 default['sublime_text']['checksum']="01baed30d66432e30002a309ff0393967be1daba5cce653e43bba6bd6c38ab84"
-default['sublime_text']['name']="Sublime Text 2"
-default['sublime_text']['install_path']="/opt/sublime_text"
+default['sublime_text']['install_path']="/opt"
+default['sublime_text']['install_dir']="sublime_text"
 
+default['sublime_text']['name']="Sublime Text 2"
 default['sublime_text']['shortcut_name']="sublime"
--- uncrate_linux_apps/recipes/sublime_text.rb
+++ uncrate_linux_apps/recipes/sublime_text.rb
@@ -1,34 +1,24 @@
 sublime_text_tar_filename = File.basename(node['sublime_text']['download_url'])
 sublime_text_tar_filepath = "#{Chef::Config['file_cache_path']}/#{sublime_text_tar_filename}"
 sublime_text_extract_path = "#{Chef::Config['file_cache_path']}/sublime_text/#{node['sublime_text']['checksum']}"
+sublime_text_install_path = "#{node['sublime_text']['install_path']}/#{node['sublime_text']['install_dir']}"
 
-remote_file sublime_text_tar_filepath do
-  source node['sublime_text']['download_url']
+include_recipe "ark"
+
+ark node['sublime_text']['install_dir'] do
+  url node['sublime_text']['download_url']
+  path node['sublime_text']['install_path']
   checksum node['sublime_text']['checksum']
-  owner "root"
-  group "root"
-  mode "0644"
-end
-
-bash "extract_sublime_text" do
-  cwd ::File.dirname(sublime_text_tar_filepath)
-  code <<-EOH
-    mkdir -p #{sublime_text_extract_path}
-    tar jxvf #{sublime_text_tar_filename} -C #{sublime_text_extract_path}
-    mkdir -p #{node['sublime_text']['install_path']}
-    mv #{sublime_text_extract_path}/*/* #{node['sublime_text']['install_path']}/
-  EOH
-
-  not_if { ::File.exists?(sublime_text_extract_path)}
+  mode 0755
+  action :put
 end
 
 template "/usr/share/applications/#{node['sublime_text']['shortcut_name']}.desktop" do
   source "sublime_text.desktop.erb"
   cookbook "uncrate_linux_apps"
-  owner node['current_user']
-  mode "0755"
+  mode "0644"
 end
 
 link "/usr/local/bin/#{node['sublime_text']['shortcut_name']}" do
-  to "#{node['sublime_text']['install_path']}/sublime_text"
+  to "#{sublime_text_install_path}/sublime_text"
 end
