c8169af0cf8fbcba37cc8aebc7d72bfd9c63c11b
John Leacox <johnlcox@gmail.com>
2013-08-04 05:28:26 -0500

    Refactor play framework recipe

--- uncrate_linux_apps/recipes/play_framework.rb
+++ uncrate_linux_apps/recipes/play_framework.rb
@@ -1,27 +1,25 @@
-include_recipe "uncrate_linux_apps::unzip"
-
 play_tar_filename = File.basename(node['play']['download_url'])
 play_tar_filepath = "#{Chef::Config['file_cache_path']}/#{play_tar_filename}"
 play_extract_path = "#{Chef::Config['file_cache_path']}/play/#{node['play']['checksum']}"
-play_install_path = "#{node['play']['install_dir']}/#{File.basename(play_tar_filename, '.*')}"
+play_install_path = "#{node['play']['install_path']}/#{node['play']['install_dir']}"
 
-remote_file play_tar_filepath do
-  source node['play']['download_url']
+include_recipe "ark"
+
+ark node['play']['install_dir'] do
+  url node['play']['download_url']
+  path node['play']['install_path']
   checksum node['play']['checksum']
-  owner "root"
-  group "root"
-  mode "0644"
+  owner node['uncrate']['user']
+  group node['uncrate']['user']
+  mode 0755
+  action :put
 end
 
-bash "extract play" do
-  cwd ::File.dirname(play_tar_filepath)
-  code <<-EOH
-    mkdir -p #{play_extract_path}
-    unzip #{play_tar_filename} -d #{play_extract_path}
-    mkdir -p #{play_install_path}
-    mv #{play_extract_path}/*/* #{play_install_path}
-    chown -R #{node['uncrate']['user']}:#{node['uncrate']['user']} #{play_install_path}
-  EOH
+link "#{node['play']['install_path']}/#{node['play']['symlink_dir']}" do
+  to play_install_path
+end
 
-  not_if { ::File.exists?(play_install_path)}
+template "#{node['uncrate']['envdir']}/play.sh" do
+  source "playenv.sh.erb"
+  mode "0755"
 end
