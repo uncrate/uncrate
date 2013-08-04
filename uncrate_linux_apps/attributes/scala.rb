20d2edc91369293bdb9bd4cc3c3de9161f834f73
John Leacox <johnlcox@gmail.com>
2013-08-04 04:37:09 -0500

    Refactor scala recipe

--- uncrate_linux_apps/attributes/scala.rb
+++ uncrate_linux_apps/attributes/scala.rb
@@ -1,3 +1,5 @@
 default['scala']['download_url']="http://www.scala-lang.org/files/archive/scala-2.10.2.tgz"
 default['scala']['checksum']="f693fdbc4944a40a080866593c70c14253fca2f553a0c50a26c8c52c489666a3"
-default['scala']['install_dir']="/opt"
+default['scala']['install_path']="/opt"
+default['scala']['install_dir']="scala-2.10.2"
+default['scala']['symlink_dir']="scala"
--- uncrate_linux_apps/recipes/scala.rb
+++ uncrate_linux_apps/recipes/scala.rb
@@ -1,44 +1,23 @@
 scala_tar_filename = File.basename(node['scala']['download_url'])
 scala_tar_filepath = "#{Chef::Config['file_cache_path']}/#{scala_tar_filename}"
 scala_extract_path = "#{Chef::Config['file_cache_path']}/scala/#{node['scala']['checksum']}"
-scala_install_path = "#{node['scala']['install_dir']}/#{File.basename(scala_tar_filename, '.*')}"
+scala_install_path = "#{node['scala']['install_path']}/#{node['scala']['install_dir']}"
 
-remote_file scala_tar_filepath do
-  source node['scala']['download_url']
+include_recipe "ark"
+
+ark node['scala']['install_dir'] do
+  url node['scala']['download_url']
+  path node['scala']['install_path']
   checksum node['scala']['checksum']
-  owner "root"
-  group "root"
-  mode "0644"
+  mode 0755
+  action :put
 end
 
-bash "extract scala" do
-  cwd ::File.dirname(scala_tar_filepath)
-  code <<-EOH
-    mkdir -p #{scala_extract_path}
-    tar zxf #{scala_tar_filename} -C #{scala_extract_path}
-    mkdir -p #{scala_install_path}
-    mv #{scala_extract_path}/* #{scala_install_path}
-  EOH
-
-  not_if { ::File.exists?(scala_install_path)}
+link "#{node['scala']['install_path']}/#{node['scala']['symlink_dir']}" do
+  to scala_install_path
 end
 
-link "/usr/bin/scala" do
-  to "#{scala_install_path}/bin/scala"
-end
-
-link "/usr/bin/scalac" do
-  to "#{scala_install_path}/bin/scalac"
-end
-
-link "/usr/bin/scaladoc" do
-  to "#{scala_install_path}/bin/scaladoc"
-end
-
-link "/usr/bin/scalap" do
-  to "#{scala_install_path}/bin/scalap"
-end
-
-link "/usr/bin/fsc" do
-  to "#{scala_install_path}/bin/fsc"
+template "#{node['uncrate']['envdir']}/scala.sh" do
+  source "scalaenv.sh.erb"
+  mode "0755"
 end
