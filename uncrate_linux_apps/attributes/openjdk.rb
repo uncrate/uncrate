fd07548361755e5a16a13350ee599833d850cd6c
John Leacox <johnlcox@gmail.com>
2013-08-14 20:33:11 -0500

    Remove java recipes

--- uncrate_linux_apps/attributes/openjdk.rb
+++
@@ -1,4 +0,0 @@
-override['java']['jdk_version'] = '7'
-
-# Workaround to force reload of the java cookbook attributes with the new jdk_version
-node.from_file(run_context.resolve_attribute(*parse_attribute_file_spec("java")))
