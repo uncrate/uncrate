3b71b3eeb5c5cd4ca3c0640ba029b7397c226ec4
John Leacox <johnlcox@gmail.com>
2013-09-26 20:14:33 -0500

    Play framework recipe moved to play_framework-uncrate project

--- uncrate_linux_apps/attributes/play_framework.rb
+++
@@ -1,5 +0,0 @@
-default['play']['download_url']="http://downloads.typesafe.com/play/2.1.2/play-2.1.2.zip"
-default['play']['checksum']="adebc13cecaaa0fb09dfadbda3191063590464633031ac80633db9f9a8c969db"
-default['play']['install_path']="/opt"
-default['play']['install_dir']="play-2.1.2"
-default['play']['symlink_dir']="play"
