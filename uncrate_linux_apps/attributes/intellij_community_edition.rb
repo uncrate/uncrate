c8ee2b57f5c74d7808b4a2a5ee248a55cea8563d
John Leacox <johnlcox@gmail.com>
2013-09-29 16:56:42 -0500

    Remove intellij recipe

--- uncrate_linux_apps/attributes/intellij_community_edition.rb
+++
@@ -1,7 +0,0 @@
-default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-12.1.4.tar.gz"
-default['intellij_community_edition']['checksum']="d1307a88263147ab537b5ed61acc878ead6ca31422dc8ac652fbb551bd9300cd"
-default['intellij_community_edition']['install_path']="/usr/local"
-default['intellij_community_edition']['install_dir']="ideaIC-12"
-
-default['intellij_community_edition']['name']="IntelliJ IDEA Community Edition"
-default['intellij_community_edition']['shortcut_name']="intellij-ideaIC"
