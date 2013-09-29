2e1ed5124573d6c6f104d951e08fb5f54598c185
John Leacox <johnlcox@gmail.com>
2013-09-29 11:32:38 -0500

    Remove maven recipe

--- uncrate_linux_apps/attributes/maven3.rb
+++
@@ -1,5 +0,0 @@
-default['maven']['download_url']="http://apache.claz.org/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz"
-default['maven']['checksum']="d98d766be9254222920c1d541efd466ae6502b82a39166c90d65ffd7ea357dd9"
-default['maven']['install_path']="/usr/local"
-default['maven']['install_dir']="apache-maven-3.0.5"
-default['maven']['symlink_dir']="maven"
